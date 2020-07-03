1b43b49a5cd8f9c77eb6dda353d17224c091e124
guancaiwang <guancaiwang@xiaoheiban.cn>
2020-07-03 16:16:17 +0800

    feat: add webpack

---
+++ webpack/test/src/a.js
@@ -0,0 +1,9 @@
+import b from "./b.js";
+b();
+
+function a() {
+  console.log("module a");
+}
+
+a();
+export default a;
---
+++ webpack/test/src/b.js
@@ -0,0 +1,4 @@
+function b() {
+  console.log("module b");
+}
+export default b;
